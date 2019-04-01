7d975def1008516c30794dcce0b497a1bc6837e9
yiyern <yiyern@yahoo.com>
2019-04-01 22:53:52 +0800

    init

--- worker/target/classes/mysql/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `userInfo`;
-CREATE TABLE `userInfo`(
-  `id` int(13) NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `name` varchar(33) DEFAULT NULL COMMENT '姓名',
-  `age` int(3) DEFAULT NULL COMMENT '年龄',
-  `money` double DEFAULT NULL COMMENT '账户余额',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
